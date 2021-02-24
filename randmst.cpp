15aebd19b069348e33daecfb195d55367021b9d5
Rucha Joshi <ruchajoshi@college.harvard.edu>
2021-02-23 20:16:58 -0500

    created adjacency matrix for dim 0

--- randmst.cpp
+++ randmst.cpp
@@ -31,7 +31,35 @@ int main(int argc, char **argv) {
     // number of random numbers
     int n = 5;
 
-    // prints n random numbers --> for graph 1
+    // Create adjacency matrix of size n x n
+    double adjMatrix[n][n];
+
+    // Fill adjacency matrix
+    for (int i = 0; i <n; i++) {
+        for (int j = 0; j<n; j++) {
+            if (i == j) {
+                adjMatrix[i][j] = 0;
+            }
+            else {
+                double x = randNum();
+                cout << x << " this is x" << endl;
+                adjMatrix[i][j] = x;
+                adjMatrix[j][i] = x;
+            }
+        }
+    }
+
+    cout << "adjacency matrix" << endl;
+
+    // Print out adjacency matrix
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < n; j++) {
+         cout << adjMatrix[i][j] << " ";
+      }
+      cout << endl;
+   }
+
+    // prints n random numbers --> for graph 1, dimension 0
     srand(static_cast<unsigned int>(clock()));
     for (int i=0; i < n; i++) {
         double x = randNum();

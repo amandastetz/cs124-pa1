ccc19569ccbe4f76d82a3ef34e2f67f1e5360946
Amanda Stetz <astetzart@gmail.com>
2021-02-25 17:00:42 -0500

    Sort vector

--- test.cpp
+++ test.cpp
@@ -1,37 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+// sort algorithm example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::sort
+#include <vector>       // std::vector
 
-double values[] = { 88, 56, 100, 2, 25 };
+bool myfunction (int i,int j) { return (i<j); }
 
-int compare (const void * a, const void * b)
-{
-    if (*(double*)a > *(double*)b) return 1;
-    else if (*(double*)a < *(double*)b) return -1;
-    else return 0;
-}
+struct myclass {
+  bool operator() (int i,int j) { return (i<j);}
+} myobject;
 
-int main()
-{
-    int n;
+int main () {
+  int myints[] = {32,71,12,45,26,80,53,33};
+  std::vector<int> myvector (myints, myints+8);               // 32 71 12 45 26 80 53 33
 
-    printf("Before sorting the list is: \n");
-    for( n = 0 ; n < 5; n++ )
-    {
-        printf("%.2f ", values[n]);
-    }
+  // using default comparison (operator <):
+  std::sort (myvector.begin(), myvector.begin()+4);           //(12 32 45 71)26 80 53 33
 
-    printf("\n\n");
+  // using function as comp
+  std::sort (myvector.begin()+4, myvector.end(), myfunction); // 12 32 45 71(26 33 53 80)
 
-    qsort(values, 5, sizeof(double), compare);
+  // using object as comp
+  std::sort (myvector.begin(), myvector.end(), myobject);     //(12 26 32 33 45 53 71 80)
 
-    printf("\nAfter sorting the list is: \n");
-    for( n = 0 ; n < 5; n++ )
-    {
-        printf("%.2f ", values[n]);
-    }
+  // print out content:
+  std::cout << "myvector contains:";
+  for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
 
-    printf("\n\n");
-
-    return(0);
+  return 0;
 }
